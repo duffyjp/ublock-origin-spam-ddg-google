e2f7bc039f09a4c8228e366acf0b4e9544bca63c
Jacob Duffy <duffy.jp@gmail.com>
2022-12-16 09:44:14 -0600

    domains.txt now alphabetizes itself when update.rb is run.

--- update.rb
+++ update.rb
@@ -1,12 +1,15 @@
 #!/usr/bin/ruby
 
 blocklist = []
+domains   = []
 
 File.open("./domains.txt", "r").read.lines.each do |line|
   next if ['#', nil].include? line.strip[0]
 
+  domains   << line.strip
   blocklist << %Q|duckduckgo.*##.results > div:has(a[href*="#{line.strip}"])|
   blocklist << %Q|google.*##.g:has(a[href*="#{line.strip}"])|
 end
 
 File.write("blocklist.txt", blocklist.sort.join("\n") + "\n")
+File.write("domains.txt", "# Add list of domains to block here.\n\n" + domains.sort.join("\n") + "\n")
